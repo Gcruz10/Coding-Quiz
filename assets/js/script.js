99fc5554ef0a4961a8751112a662f2cae7429ce4
Gcruz10 <gabrielcruz2335@gmail.com>
2023-08-27 23:05:53 -0400

    added stlying

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ var answers = [
     document.getElementById("3"),
     document.getElementById("4")
   ];
+
 var timeLeft = 75;
 var score = 0;
 var questionIndex = 0;
@@ -18,6 +19,7 @@ function quizTimer(){
     }, 1000);
     
 }
+
 var questionArr = [
     "Which data type will show either True or False?",
     "Which one of these will put information in the console",
@@ -36,24 +38,49 @@ var answerArr = {
 
 function questionMaker(questionText, answersArr) {
     question.textContent = questionText;
-    for (var x = 0; x < answersArr.length; x++) {
-        answers[x].textContent = x + 1 + "." + answersArr[x]
-        answers[x].addEventListener("click", function(event) {
-            if (event.target.textContent === "Boolean") {
-                score++;
-                questionIndex++;
-                questionMaker(questionArr[questionIndex], answerArr[questionIndex]);
+    for (var x = 0; x < answers.length; x++) {
+        answers[x].removeEventListener("click", answerClickHandler);
+    }
+
+    function answerClickHandler(event) {
+        var selectedAnswer = event.target.textContent.substr(3);
+
+        if (selectedAnswer === answersArr[questionIndex][0]) {
+            score++;
+        }
+
+        questionIndex++;
+
+        if (questionIndex < questionArr.length) {
+            questionMaker(questionArr[questionIndex], answerArr[questionIndex]);
+        } else {
+            var quizFinishedSection = document.getElementById("quiz-finished");
+            var finalScoreSpan = document.getElementById("final-score");
+            quizFinishedSection.style.display = "block";
+            finalScoreSpan.textContent = score;
+
+            for (var x = 0; x < answers.length; x++) {
+                answers[x].removeEventListener("click", answerClickHandler);
             }
-            else {
-                questionIndex++;
-                questionMaker(questionArr[questionIndex], answerArr[questionIndex]);
-            }
-        })
+        }
+    }
+    for (var x = 0; x < answers.length; x++) {
+        answers[x].addEventListener("click", answerClickHandler);
+        answers[x].textContent = x + 1 + "." + answersArr[x];
     }
 }
+
 questionMaker(questionArr[0], answerArr[0]);
 
-startButton.addEventListener("click", function() {
-  quizTimer();
-});
 
+var submitScoreButton = document.getElementById("submit-score");
+var initialsInput = document.getElementById("initials-input");
+submitScoreButton.addEventListener("click", function() {
+    var initials = initialsInput.value.trim();
+
+    if (initials !== "") {
+        var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+        highScores.push({ initials: initials, score: score });
+        localStorage.setItem("highScores", JSON.stringify(highScores));
+    }
+});
